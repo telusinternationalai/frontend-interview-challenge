3f4ab4948381fccb46a013d83cd040c78d5cda2a
Romaine Allen <romallen1@gmail.com>
2022-05-26 18:54:18 +0900

    Imlplement page fetch

--- src/views/ListView.js
+++ src/views/ListView.js
@@ -1,10 +1,32 @@
 import { useEffect, useState } from "react";
-
+import { Box, Container } from "@material-ui/core";
+import axios from "axios";
 export default function ListView() {
+  const [pageData, setPageData] = useState([]);
 
-return (
-    <div>
-        <h1>ListView</h1>
-    </div>
-)
+  useEffect(() => {
+    fetchPageData();
+  }, []);
+
+  const fetchPageData = (num = 1) => {
+    axios
+      .get(`http://localhost:3000/persons?page=${1}`)
+      .then((res) => {
+
+        setPageData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+//   useEffect(() => {
+//     console.log(pageData.results[0].id);
+//   }, [pageData]);
+
+  return (
+    <Container>
+    <Box>Listview</Box>
+
+    </Container>
+  );
 }
